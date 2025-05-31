cb5a150b833256fa2d39303b27854c0b8c9186cf
GodreignElgin <153350008+GodreignElgin@users.noreply.github.com>
2025-05-31 20:58:52 +0530

    sample added

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const permissionRoutes = require('./permissionRoutes');
+const productRoutes = require('./productRoutes');
 
 // Mount the routes
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/', permissionRoutes);  // Since permission routes already have full paths
+// sample
+// router.use('/products', productRoutes);
 
 module.exports = router;  // Export the router, not an object
